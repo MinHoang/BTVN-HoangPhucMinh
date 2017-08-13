4d8ff33bba0bd7a49d9091f264646998b4cc66ad
DESKTOP-F9FJEK4\hoang <hoangphucminz@gmail.com>
2017-08-13 22:52:36 +0700

    S7 Homework

---
+++ Homework S7/Convert/Convert/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Convert
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int original_number = 123456789;
+			int converted_number;
+			int devided_number;
+			while (true) 
+
+			{
+				converted_number = (original_number % 2);
+				devided_number = (original_number / 2);
+				Console.Write (converted_number);
+				original_number = devided_number;
+				if (devided_number == 0) 
+				{
+					Console.WriteLine ();
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ Homework S7/UCLN/UCLN/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UCLN
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int j; 
+			int smallest, largest; 
+			int num; 
+
+			num = 100; 
+
+			largest = 1; 
+
+			for(j=num/2;(j >= 2); j--) { 
+
+				if((largest == 1) & ((num % j) == 0))  
+					largest = j; 
+
+			} 
+
+			Console.WriteLine("Uoc So Lon Nhat: " + largest); 
+		}
+	}
+}
