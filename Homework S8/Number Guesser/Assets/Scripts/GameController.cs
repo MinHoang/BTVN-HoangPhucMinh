3e76bf4cc4fe2ed0e5cd7edb50b58d70e8487a8c
DESKTOP-F9FJEK4\hoang <hoangphucminz@gmail.com>
2017-08-17 22:55:50 +0700

    Homework S8

---
+++ Homework S8/Number Guesser/Assets/Scripts/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public Text displaytext;
+	public Button HigherButton;
+	public Button LowerButton;
+	public Button YesButton;
+	int RandomNumber;
+	int PickedNumber;
+	int ResultNumber = 2;
+
+	// Use this for initialization
+	void Start () {
+		RandomNumber = Random.Range (0, 100);
+		PickedNumber = RandomNumber;
+		displaytext.text = "Is " + RandomNumber + " Your Number?";
+		NumberRangeChanger ();
+	}
+	public void higherbutton()
+	{
+		ResultNumber = 1;
+			NumberRangeChanger();
+		
+	}
+	public void lowerbutton()
+	{
+		ResultNumber = -1;
+			NumberRangeChanger();
+
+	}
+	public void yesbutton(){
+		ResultNumber = 0;
+			displaytext.text ="I Got It!";
+	}
+
+	public void NumberRangeChanger()
+	{
+		if (ResultNumber == 1) {
+			RandomNumber = Random.Range (PickedNumber, 100);
+			PickedNumber = RandomNumber;
+			displaytext.text = "Is " + RandomNumber + " Your Number?";
+		} else if (ResultNumber == -1) {
+			RandomNumber = Random.Range (0, PickedNumber);
+			PickedNumber = RandomNumber;
+			displaytext.text = "Is " + RandomNumber + " Your Number?";
+		} else if (ResultNumber == 0) {
+			print ("I Got It!");
+			
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
